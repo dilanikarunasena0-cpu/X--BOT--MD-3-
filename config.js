982cf18f386c07ce196dbc0f87d2199f210fc660
isankamalith74-dotcom <isankamalith74@gmail.com>
2026-06-12 18:44:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -67,7 +67,7 @@ module.exports = {
 	READ_MESSAGES: toBool(process.env.READ_MESSAGES || "true"),
 	REJECT_CALL: toBool(process.env.REJECT_CALL || "false"),
 	REJECT_CALL_MSG: process.env.REJECT_CALL_MSG || "_Calls are not allowed. Please don’t call again!._",
-	SESSION_ID: process.env.SESSION_ID || "",
+	SESSION_ID: process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:ec8de669097ad9d448dbd823526b3c8b",
 	START_MSG: toBool(process.env.START_MSG || "true"),
 	STICKER_DATA: process.env.STICKER_DATA || `X BOT MD;ASWIN SPARKY`,
 	
